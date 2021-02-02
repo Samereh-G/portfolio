f21b9429421e24a820a50369b12d9f207d5f9d80
F-Gholami <fereshteh_gholami@outlook.com>
2021-02-02 09:59:28 +0100

    added all files befor deploying

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import 'animate.css/animate.min.css'
 import { CssBaseline } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/core/styles'
 import React from 'react'
-import MainScreen from './screens'
+// import { BrowserRouter } from 'react-router-dom'
 import theme from './theme'
+// import MainRoute from './routes/MainRoute'
+import MainScreen from './screens/MainScreen'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      {/* <BrowserRouter> */}
       <CssBaseline />
       <MainScreen />
+      {/* </BrowserRouter> */}
     </ThemeProvider>
   )
 }
